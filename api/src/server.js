a722fc39850d920373a40c584183c5eef5dd2827
Daniel Reszka <dannn.r@gmail.com>
2019-10-24 02:29:19 +0200

    API: add minimal GraphQL server

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,47 @@
 import express from "express";
+import yoga from "graphql-yoga";
 import path from "path";
 
-const app = express();
-const port = process.env.PORT || 3100;
-const UI = path.resolve("../ui/dist");
+const { GraphQLServer } = yoga;
 
-app.use(express.static(UI));
+const env = {
+  API_PORT: process.env.PORT || 3100,
+  GRAPHQL_ENDPOINT: "/graphql"
+};
 
-app.get("*", (req, res) => res.sendFile(path.join(UI, "index.html")));
+const UI_DIR = path.resolve("../ui/dist");
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || "World"}`
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers, endpoint: "graphql" });
+
+server.express.use(express.static(UI_DIR));
+
+server.express.get("*", (req, res, next) =>
+  req.url === env.GRAPHQL_ENDPOINT
+    ? next()
+    : res.sendFile(`${UI_DIR}/index.html`)
+);
+
+server.start(
+  {
+    port: env.API_PORT,
+    endpoint: env.GRAPHQL_ENDPOINT,
+    playground: env.GRAPHQL_ENDPOINT,
+    subscriptions: env.GRAPHQL_ENDPOINT
+  },
+  () =>
+    console.log(
+      `Server running on http://localhost:${env.API_PORT}${env.GRAPHQL_ENDPOINT}`
+    )
+);
