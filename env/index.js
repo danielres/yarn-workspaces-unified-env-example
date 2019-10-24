3dd982b003670c53c734693edf961e3f372408da
Daniel Reszka <dannn.r@gmail.com>
2019-10-24 02:59:45 +0200

    ENV: create new yarn workspace "env"
    * this will contain shared env configuration for
    API + UI

---
+++ env/index.js
@@ -0,0 +1,13 @@
+import envalid from "envalid";
+const { str, email, json, num } = envalid;
+
+const env = envalid.cleanEnv(
+  process.env,
+  {
+    API_PORT: num({ default: 3100 }),
+    API_GRAPHQL_ENDPOINT: str({ default: "/graphql" })
+  },
+  { strict: true }
+);
+
+export default env;
