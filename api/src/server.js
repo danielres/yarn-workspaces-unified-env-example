17e2b8252b4acc370352c405ccbab4f2e2ca41b0
Daniel Reszka <dannn.r@gmail.com>
2019-10-24 03:00:18 +0200

    API: use shared config from env workspace

--- api/src/server.js
+++ api/src/server.js
@@ -1,13 +1,9 @@
+import env from "env";
 import express from "express";
 import yoga from "graphql-yoga";
 import path from "path";
 
 const { GraphQLServer } = yoga;
-
-const env = {
-  API_PORT: process.env.PORT || 3100,
-  GRAPHQL_ENDPOINT: "/graphql"
-};
 
 const UI_DIR = path.resolve("../ui/dist");
 
@@ -28,7 +24,7 @@ const server = new GraphQLServer({ typeDefs, resolvers, endpoint: "graphql" });
 server.express.use(express.static(UI_DIR));
 
 server.express.get("*", (req, res, next) =>
-  req.url === env.GRAPHQL_ENDPOINT
+  req.url === env.API_GRAPHQL_ENDPOINT
     ? next()
     : res.sendFile(`${UI_DIR}/index.html`)
 );
@@ -36,12 +32,12 @@ server.express.get("*", (req, res, next) =>
 server.start(
   {
     port: env.API_PORT,
-    endpoint: env.GRAPHQL_ENDPOINT,
-    playground: env.GRAPHQL_ENDPOINT,
-    subscriptions: env.GRAPHQL_ENDPOINT
+    endpoint: env.API_GRAPHQL_ENDPOINT,
+    playground: env.API_GRAPHQL_ENDPOINT,
+    subscriptions: env.API_GRAPHQL_ENDPOINT
   },
   () =>
     console.log(
-      `Server running on http://localhost:${env.API_PORT}${env.GRAPHQL_ENDPOINT}`
+      `Server running on http://localhost:${env.API_PORT}${env.API_GRAPHQL_ENDPOINT}`
     )
 );
