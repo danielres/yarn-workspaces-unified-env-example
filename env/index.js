c3120d5a1dce03386088d6284aba5d2326e9ef79
Daniel Reszka <dannn.r@gmail.com>
2019-10-25 13:33:36 +0200

    ENV: add support for .env.*.local files

--- env/index.js
+++ env/index.js
@@ -1,4 +1,14 @@
 import { default as envalid, num, str } from "envalid";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+if (!process.env.NODE_ENV) process.env.NODE_ENV = "development";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const dotEnvFile = ["development", "test"].includes(process.env.NODE_ENV)
+  ? `.env.${process.env.NODE_ENV}.local`
+  : null;
 
 const env = envalid.cleanEnv(
   process.env,
@@ -7,7 +17,10 @@ const env = envalid.cleanEnv(
     API_PORT: num({ default: process.env.PORT || 3100 }), // process.env.PORT is for Heroku
     UI_DEV_SERVER_PORT: num({ default: 1234 })
   },
-  { strict: true }
+  {
+    dotEnvPath: path.join(__dirname, dotEnvFile),
+    strict: true
+  }
 );
 
 export default env;
