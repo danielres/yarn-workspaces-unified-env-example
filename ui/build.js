91ec82c5072a4b8f96fbaefa7ac53f9c48503943
Daniel Reszka <dannn.r@gmail.com>
2019-10-29 00:32:13 +0100

    UI: reduce bundle size > 50% with parcel tree-shaking

--- ui/build.js
+++ ui/build.js
@@ -3,7 +3,8 @@ import Bundler from "parcel-bundler";
 const bundler = new Bundler("src/index.html", {
   cache: false,
   autoInstall: false,
-  watch: false
+  watch: false,
+  scopeHoist: true
 });
 
 bundler.bundle();
