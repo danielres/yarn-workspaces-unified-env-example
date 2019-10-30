0efe8647712911a27d494009128b8478e121ca2a
Daniel Reszka <dannn.r@gmail.com>
2019-10-31 00:34:25 +0100

    UI: improve build script
    * enables tree-shaking (reduces bundle size > 50%)
    * enables build detailed report in terminal
    * ensures minification enabled

--- ui/build.js
+++ ui/build.js
@@ -4,7 +4,9 @@ const bundler = new Bundler("src/index.html", {
   cache: false,
   autoInstall: false,
   watch: false,
-  scopeHoist: true
+  scopeHoist: true, // tree-shaking (experimental )
+  minify: true,
+  detailedReport: true
 });
 
 bundler.bundle();
