25791ad8c7558052e21cbaba2aa6148eb75f4fe5
Daniel Reszka <dannn.r@gmail.com>
2019-10-31 00:38:44 +0100

    UI: enable faster refreshes in dev with HMR

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -6,6 +6,8 @@ import "./global.css";
 import App from "./scenes/App";
 import { Providers } from "./services";
 
+if (module.hot) module.hot.accept();
+
 ReactDOM.render(
   <Providers>
     <App />
