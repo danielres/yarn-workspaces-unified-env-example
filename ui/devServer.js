63e4873c271c5f46d4f75163743fc709f901e8f2
Daniel Reszka <dannn.r@gmail.com>
2019-11-08 11:18:15 +0100

    UI: improve parcel cache handling in dev vs test
    * enables  parcel cache in test
    * simplifies cache handling

--- ui/devServer.js
+++ ui/devServer.js
@@ -2,9 +2,6 @@ import express from "express";
 import proxy from "http-proxy-middleware";
 import Bundler from "parcel-bundler";
 
-const [, , ...args] = process.argv;
-const isCacheEnabled = args.includes("cache:true");
-
 const config = {
   devServerPort: process.env.UI_DEV_SERVER_PORT,
   endpoint: process.env.API_GRAPHQL_ENDPOINT,
@@ -12,7 +9,8 @@ const config = {
 };
 
 const bundler = new Bundler("src/index.html", {
-  cache: isCacheEnabled,
+  cache: true,
+  cacheDir: `.cache/${process.env.NODE_ENV}`,
   autoInstall: false
 });
 
@@ -29,7 +27,5 @@ app.use(
 app.use(bundler.middleware());
 
 app.listen(config.devServerPort, () => {
-  console.log(
-    `[UI:devServer] ${process.env.NODE_ENV} | ${args} | http://localhost:${config.devServerPort}`
-  );
+  console.log(`[UI] devServer: http://localhost:${config.devServerPort}`);
 });
