2c2a597d5b5590f1f2cad59e4d9be3d3e57d6406
Daniel Reszka <dannn.r@gmail.com>
2019-11-21 12:37:45 +0100

    API: add a short cache to persistUser
    * reduces multiple user db queries generated by middlewares
    to 1 per request

--- api/src/middlewares/persistUser/index.js
+++ api/src/middlewares/persistUser/index.js
@@ -1,26 +1,33 @@
 import { User } from "db/queries";
+import NodeCache from "node-cache";
+
+const dbUserCache = new NodeCache({ maxKeys: 100, stdTTL: 5 });
 
 export default async (resolve, parent, args, ctx, info) => {
   const { user } = ctx;
 
   let dbUser;
 
-  dbUser = await User.findBySub(user.sub);
-
-  if (!dbUser) {
-    dbUser = await User.create({
-      sub: user.sub,
-      name: user.name,
-      email: user.email
-    });
+  if (!dbUserCache.get(user.sub)) {
+    dbUser = await User.findBySub(user.sub);
+    if (!dbUser) {
+      dbUser = await User.create({
+        sub: user.sub,
+        name: user.name,
+        email: user.email
+      });
+    }
+    dbUserCache.set(user.sub, dbUser);
   }
 
+  const cached = dbUserCache.get(user.sub);
+
   ctx.user = {
-    id: dbUser.id,
-    name: dbUser.name,
-    email: dbUser.name,
-    createdAt: dbUser.createdAt,
-    updatedAt: dbUser.updatedAt
+    id: cached.id,
+    name: cached.name,
+    email: cached.name,
+    createdAt: cached.createdAt,
+    updatedAt: cached.updatedAt
   };
 
   return resolve();
