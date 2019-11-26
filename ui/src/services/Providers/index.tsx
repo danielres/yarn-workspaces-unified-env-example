efd18f7d0fd7aa07c0df409d90e84dc8a2f3b39c
Daniel Reszka <dannn.r@gmail.com>
2019-11-26 17:37:17 +0100

    UI: refactor providers + make compatible with e2e tests
    * simplifies providers
    
    * fixes previous implementation that was preventing the
    direct user/password +
    access-token auth needed by e2e tests

--- ui/src/services/Providers/index.tsx
+++ ui/src/services/Providers/index.tsx
@@ -1,25 +1,12 @@
 import * as React from "react";
-import Login from "../../components/Login";
-import { useAuth0 } from "../react-auth0-spa";
 import AppStateProvider from "./AppStateProvider";
 import Auth0Provider from "./Auth0Provider";
-import GraphqlProviderWithAuth from "./GraphqlProviderWithAuth";
+import GraphqlProvider from "./GraphqlProvider";
 
 export default ({ children }) => (
   <Auth0Provider>
-    <AuthGate>
-      <GraphqlProviderWithAuth>
-        <AppStateProvider>{children}</AppStateProvider>
-      </GraphqlProviderWithAuth>
-    </AuthGate>
+    <GraphqlProvider>
+      <AppStateProvider>{children}</AppStateProvider>
+    </GraphqlProvider>
   </Auth0Provider>
 );
-
-function AuthGate({ children }) {
-  const { isAuthenticated, loading } = useAuth0();
-
-  if (loading) return "Authenticating...";
-  if (isAuthenticated) return children;
-
-  return <Login />;
-}
